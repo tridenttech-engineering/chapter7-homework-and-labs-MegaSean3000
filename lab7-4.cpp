e685c76e21607c79fc6351a8768c8cc8ea507ebb
MegaSean3000 <sfp1216@gmail.com>
2025-02-19 17:47:35 -0500

    Update lab7-4.cpp

--- lab7-4.cpp
+++ lab7-4.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 int main()
 {
+    double avgBill = 0.0;
+    double bill = 0.0;
+    double totalBills = 0.0;
+    int months = 0;
 
-	return 0;
-}	//end of main function
+    cout << "Bill for month 1: ";
+    cin >> bill;
+
+    while (bill >= 0.0)
+    {
+        totalBills += bill;
+        months += 1;
+
+        cout << "Bill for month " << months + 1 << ": ";
+        cin >> bill;
+    }
+
+    if (months > 0)
+    {
+        avgBill = totalBills / months;
+        cout << fixed << setprecision(2);
+        cout << "Average electric bill for " << months << " months: $" << avgBill << endl;
+    }
+    else
+    {
+        cout << "No bill amount entered." << endl;
+    }
+
+    return 0;
+} // end of main function
