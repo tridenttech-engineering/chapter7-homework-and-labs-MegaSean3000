3f4e0c56693cf043f18f2b745f4641f6bcccef74
MegaSean3000 <sfp1216@gmail.com>
2025-02-19 16:35:27 -0500

    Update hw7-16.cpp

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -9,18 +9,22 @@ int main()
 {
 	double bill = 0.0;
 	double tip = 0.0;
+	double rate = 0.1;
 
 	cout << "Bill amount: ";
 	cin >> bill;
 
 	cout << fixed;
-	for (double rate = 0.1; rate <= 0.2; rate += 0.05)
+	while (rate <= 0.2)
 	{
 		tip = bill * rate;
 		cout << setprecision(0);
 		cout << rate * 100 << "% tip: ";
 		cout << setprecision(2);
 		cout << "$" << tip << endl;
-	}   // end for
+
+		rate += 0.05;
+
+	}   // end while
 	return 0;
 }	//end of main function
