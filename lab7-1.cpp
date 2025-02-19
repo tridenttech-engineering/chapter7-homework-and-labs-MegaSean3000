8c72d1b09ae2e0b45fce4ab63a956bdfd8d9725b
MegaSean3000 <sfp1216@gmail.com>
2025-02-19 17:57:06 -0500

    Update lab7-1.cpp

--- lab7-1.cpp
+++ lab7-1.cpp
@@ -7,27 +7,22 @@ using namespace std;
 
 int main()
 {
-int day = 0;
-int totalTexts = 0;
-int dailyTexts = 0;
-double average = 0.0;
+    int day = 0;
+    int totalTexts = 0;
+    int dailyTexts = 0;
+    double average = 0.0;
+    const int totalDays = 7; // Total number of days
 
-for (day = 1; day < 8; day += 1){
-    cout << "How many text messages did you send on day "
-    << day << "?";
-    cin >> dailyTexts;
-    totalTexts = totalTexts + dailyTexts;
+    for (day = 1; day <= totalDays; day += 1){
+        cout << "How many text messages did you send on day " << day << "? ";
+        cin >> dailyTexts;
+        totalTexts += dailyTexts;
+    }
 
+    average = totalTexts / static_cast<double>(totalDays); // Use 7.0 for floating-point division
+    cout << fixed << setprecision(0);
+    cout << endl << "You sent approximately " << average << " text messages per day." << endl;
+
+    return 0;
 }
 
-average = totalTexts / (day - 1);
-cout << fixed << setprecision(0);
-cout << endl << "You sent aproximately " << average << " text messages per day." << endl;
-
-
-
-
-
-
-	return 0;
-}	//end of main function
